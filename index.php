e0f7269868be04a2cacd5ccd7a1539cd0f0a4f88
sabrina-fontana <sabfon@hotmail.it>
2021-03-01 17:08:21 +0100

    esercizio completo

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;600&display=swap" rel="stylesheet">
@@ -14,9 +15,12 @@
       <div class="header-top">
         <div class="header-top-left">
           <img src="img/googlelogo.svg" alt="">
-
+          <span>Privacy & Terms</span>
         </div>
-        <div class="header-top-right"></div>
+        <div class="header-top-right">
+          <i class="fas fa-bars"></i>
+          <img src="img/user.png" alt="">
+        </div>
       </div>
 
       <nav>
@@ -37,9 +41,9 @@
         $faqs = [
           [
             'question' => "How are you implementing the recent Court of Justice of the European Union (CJEU) decision on the right to be forgotten?",
-            'answer' => "<p>The recent ruling by the Court of Justice of the European Union has profound consequences for search engines in Europe. The court found that certain users have the right to ask search engines like Google to remove results for queries that include the person's name. To qualify, the results shown would need to be inadequate, irrelevant, no longer relevant, or excessive.</p>
+            'answer' => "<p>The recent <span class=\"special-text\">ruling by the Court of Justice of the European Union</span> has profound consequences for search engines in Europe. The court found that certain users have the right to ask search engines like Google to remove results for queries that include the person's name. To qualify, the results shown would need to be inadequate, irrelevant, no longer relevant, or excessive.</p>
             <p>Since this ruling was published on 13 May 2014, we've been working round the clock to comply. This is a complicated process because we need to assess each individual request and balance the rights of the individual to control his or her personal data with public's right to know and distribute information.</p>
-            <p>If you have a removal request, please fill out this webform. You'll receive an automatic reply confirming that we have received your request. We will then assess your case—please note this may take some time because we have already received many such requests. In evaluating your request, we will look at whether the results include outdated information about your private life. We'll also look at whether there's a public interest in the information remaining in our search results—for example, if it relates to financial scams, professional malpractice, criminal convictions or your public conduct as a government official (elected or unelected). These are difficult judgements and as a private organization, we may not be in a good position to decide on your case. If you disagree with our decision you can contact your local DPA.</p>
+            <p>If you have a removal request, please fill out this <span class=\"special-text\">webform</span>. You'll receive an automatic reply confirming that we have received your request. We will then assess your case—please note this may take some time because we have already received many such requests. In evaluating your request, we will look at whether the results include outdated information about your private life. We'll also look at whether there's a public interest in the information remaining in our search results—for example, if it relates to financial scams, professional malpractice, criminal convictions or your public conduct as a government official (elected or unelected). These are difficult judgements and as a private organization, we may not be in a good position to decide on your case. If you disagree with our decision you can contact your local DPA.</p>
             <p>We look forward to working closely with data protection authorities and others  over the coming months as we refine our approach. The CJEU's ruling constitutes a significant change for search engines. While we are concerned about its impact, we also believe it's important to respect the Court's judgment and are working hard to devise a process that complies with the law.</p>
             <p>When you search for a name, you may see a notice that says that results may have been modified in accordance with data protection law in Europe. We’re showing this notice in Europe when a user searches for most names, not just pages that have been affected by a removal.</p>"
           ],
@@ -47,8 +51,8 @@
             'question' => "How does Google protect my privacy and keep my information secure?",
             'answer' => "<p>We know security and privacy are important to you – and they are important to us, too. We make it a priority to provide strong security and give you confidence that your information is safe and accessible when you need it.</p>
             <p>We’re constantly working to ensure strong security, protect your privacy, and make Google even more effective and efficient for you. We spend hundreds of millions of dollars every year on security, and employ world-renowned experts in data security to keep your information safe. We also built easy-to-use privacy and security tools like Google Dashboard, 2-step verification and Ads Settings. So when it comes to the information you share with Google, you’re in control.</p>
-            <p>You can learn more about safety and security online, including how to protect yourself and your family online, at the Google Safety Center.<br><br>
-            Learn more about how we keep your personal information private and safe — and put you in control.</p>"
+            <p>You can learn more about safety and security online, including how to protect yourself and your family online, at the <span class=\"special-text\">Google Safety Center</span>.<br><br>
+            <span class=\"special-text\">Learn more</span> about how we keep your personal information private and safe — and put you in control.</p>"
           ],
           [
             'question' => "Why is my account associated with a country?",
@@ -65,11 +69,11 @@
           ],
           [
             'question' => "How can I remove information about myself from Google's search results?",
-            'answer' => "<p>Google search results are a reflection of the content publicly available on the web. Search engines can't remove content directly from websites, so removing search results from Google wouldn't remove the content from the web. If you want to remove something from the web, you should contact the webmaster of the site the content is posted on and ask him or her to make a change. Additionally, if under European data protection law, you would like to request removal of certain information about you that appears in Google's search results, please click here. Once the content has been removed and Google has noted the update, the information will no longer appear in Google's search results. If you have an urgent removal request, you can also visit our help page for more information.</p>"
+            'answer' => "<p>Google search results are a reflection of the content publicly available on the web. Search engines can't remove content directly from websites, so removing search results from Google wouldn't remove the content from the web. If you want to remove something from the web, you should <span class=\"special-text\">contact the webmaster</span> of the site the content is posted on and ask him or her to make a change. Additionally, if under European data protection law, you would like to request removal of certain information about you that appears in Google's search results, please <span class=\"special-text\">click here</span>. Once the content has been removed and Google has noted the update, the information will no longer appear in Google's search results. If you have an urgent removal request, you can also <span class=\"special-text\">visit our help page for more information</span>.</p>"
           ],
           [
             'question' => "Are my search queries sent to websites when I click on Google Search results?",
-            'answer' => "<p>In some cases, yes. When you click on a search result in Google Search, your web browser also may send the Internet address, or URL, of the search results page to the destination webpage as the Referrer URL. The URL of the search results page may sometimes contain the search query you entered. If you are using SSL Search (Google’s encrypted search functionality), under most circumstances, your search terms will not be sent as part of the URL in the Referrer URL. There are some exceptions to this behavior, such as if you are using some less popular browsers. More information on SSL Search can be found here. Search queries or information contained in the Referrer URL may be available via Google Analytics or an application programming interface (API). In addition, advertisers may receive information relating to the exact keywords that triggered an ad click.</p>"
+            'answer' => "<p>In some cases, yes. When you click on a search result in Google Search, your web browser also may send the Internet address, or URL, of the search results page to the destination webpage as the <span class=\"special-text\">Referrer URL</span>. The URL of the search results page may sometimes contain the search query you entered. If you are using SSL Search (Google’s encrypted search functionality), under most circumstances, your search terms will not be sent as part of the URL in the Referrer URL. There are some exceptions to this behavior, such as if you are using some less popular browsers. More information on SSL Search can be found <span class=\"special-text\">here</span>. Search queries or information contained in the Referrer URL may be available via Google Analytics or an application programming interface (API). In addition, advertisers may receive information relating to the exact keywords that triggered an ad click.</p>"
           ]
         ];
 
@@ -86,6 +90,22 @@
       ?>
 
     </main>
+
+    <footer>
+      <div class="footer-left">
+        <ul>
+          <li>Google</li>
+          <li>About Google</li>
+          <li>Privacy</li>
+          <li>Terms</li>
+        </ul>
+      </div>
+      <div class="footer-right">
+        <select>
+          <option value="">English</option>
+        </select>
+      </div>
+    </footer>
 
   </body>
 </html>
